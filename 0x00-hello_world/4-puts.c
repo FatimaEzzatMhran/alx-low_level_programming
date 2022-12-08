a8fd5b7f0a3ff3295b977ff03d41a02dc316fa9f
FatimaEzzatMhran <f.ezzat94@gmail.com>
2022-12-08 04:51:35 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-/* main function */
+
+/**
+ * main function
+ * it describes where we should start
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
