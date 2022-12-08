71beadbcd86f032019148ed461261f4d72b711e2
FatimaEzzatMhran <f.ezzat94@gmail.com>
2022-12-08 04:59:08 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,10 @@
-/*
- ** File: 4-puts.c
- ** Auth: Fatima Ezzat
-*/
-
 #include <stdio.h>
-
 /**
- * main function
- * it describes where we should start
- * Return (0)
-*/
-
+ ** main - main block
+ ** Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		return (0);
 }
