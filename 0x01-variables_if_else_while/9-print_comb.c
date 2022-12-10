f2ba8b7162df5dd815f5a88ccb83df6b3d66fc1b
FatimaEzzatMhran <f.ezzat94@gmail.com>
2022-12-09 16:45:48 -0800

    9. Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: Print all numbers of base 10, starting from 0.
- * You can only use the putchar function
- * Without char variable
- * num + '0' is the same as num + 48. When num = 0, the expression
- * evaluates to 48 so a 0 is outputted
+ * Description: Print all possible combinations of single-digit numbers
  * Return: 0
  */
 int main(void)
