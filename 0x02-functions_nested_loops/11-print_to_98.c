59ca4683e6b1c3daade87d1b6c5db3d5fa0599e8
FatimaEzzatMhran <f.ezzat94@gmail.com>
2022-12-13 15:02:06 -0800

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Print all natural numbers from n to 98
+ * @n: The number to begin counting at
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
