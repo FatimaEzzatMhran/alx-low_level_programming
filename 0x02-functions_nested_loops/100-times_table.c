0c1f0c0d1e15c6732ab1c11a246bb6096ffd96ac
FatimaEzzatMhran <f.ezzat94@gmail.com>
2022-12-13 15:50:17 -0800

    12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,7 @@ void print_times_table(int n)
 {
 	int first_number, second_number, result;
 
-	if (n >= 0 && n <= 15)
+	if (n > 15 ||  n < 0)
 	{
 		for (first_number = 0; first_number <= n; first_number++)
 		{
@@ -16,29 +16,36 @@ void print_times_table(int n)
 
 			for (second_number = 1; second_number <= n; second_number++)
 			{
-				_putchar(',');
-				_putchar(' ');
-
 				result = first_number * second_number;
 
-				if (result <= 99)
-                                        _putchar(' ');
-
-				if (result <= 9)
-					_putchar(' ');
-
-				if (result >= 100)
+				if (result > 99)
 				{
 					_putchar((result / 100) + '0');
-					_putchar(((result / 10)) % 10 + '0');
+					_putchar(((result / 10) % 10) + '0');
+					_putchar((result % 10) + '0');
 				}
-				else if (result <= 99 && result >= 10)
+				else if (result > 9)
 				{
+					_putchar(' ');
 					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
 				}
-				_putchar((result % 10) + '0');
+				else if (second_number != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(result + '0');
+				}
+				else
+					_putchar(result + '0');
+
+				if (second_number != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
-			_putchar((result % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
