e558e7d8d35e9c984c267798e0ab518b260ee60a
FatimaEzzatMhran <f.ezzat94@gmail.com>
2022-12-13 12:20:54 -0800

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_ahplabet_x10 - print alphabet in lowecase 10 times
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char letter;
+
+	while (count < 10)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putcahr('\n');
+		count++;
+	}
+}
