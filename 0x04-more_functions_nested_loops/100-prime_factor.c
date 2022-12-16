1444c2d4e87b96f765fcabfd47eeaf3555d19292
FatimaEzzatMhran <f.ezzat94@gmail.com>
2022-12-15 16:17:03 -0800

    advanced tasks

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - main block
+ * Description: Find and print the largest prime facto of the number
+ * 612852475143
+ * Return: 0
+ * got the answer but didn't get it -perhaps we will understand it later-
+ * int main(void)
+{
+	int c;
+	long num = 612852475143;
+
+	for (c = (int) sqrt(num); c > 2; c++)
+	{
+		if (num % c == 0)
+		{
+			printf("%d\n", c);
+			break;
+		}
+	}
+
+	return (0);
+}
+Footer
+*/
+
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/** print_number -Function that prints integer
+ * Description: Can only use _putchar to print
+ * @n: input integer parameter
+ * didn't understand the answer
+ *
+ * void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
+*/
