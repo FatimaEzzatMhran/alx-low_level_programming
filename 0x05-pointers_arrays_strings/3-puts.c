61b7d2306de3da39b81f882cbb9ab3fbfa110b00
FatimaEzzatMhran <f.ezzat94@gmail.com>
2022-12-19 12:40:49 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string followed by a new line
+ * @str: the string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++
+	}
+	_putchar('/n');
+}
