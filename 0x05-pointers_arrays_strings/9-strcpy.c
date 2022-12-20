4293de4f899933314e1f4ff8be063fe42b726cd2
FatimaEzzatMhran <f.ezzat94@gmail.com>
2022-12-19 16:54:54 -0800

    edited

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,7 +7,7 @@
  * Description: Copy the string pointed to by pointer 'src' to the buffer
  * pointed to by 'dest'
  * Return: Pointer to 'dest'
- */
+*/
 char *_strcpy(char *dest, char *src)
 {
 	int i;
