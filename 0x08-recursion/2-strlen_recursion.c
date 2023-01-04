6ac81b4a5a9ec89b812d8198a5856781cab93bcd
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-01-04 14:04:21 -0800

    strlen recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int _strlen_recursion - return length of string
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int c;
+
+	c = 0;
+	if (*s == '\0')
+		return (0);
+
+	c = 1 + _strlen_recursion(s + 1);
+
+	return (c);
+}
