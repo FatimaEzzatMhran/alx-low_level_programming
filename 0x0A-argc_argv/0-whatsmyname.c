3e51def715ad6918beed77f673893aae24e9c066
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-01-06 13:33:56 -0800

    whats my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,14 +3,18 @@
 
 /**
  * main - print the name of the file followed by a new line
- * @argc: argument count
+ * @argc: argument count - instead of writing void(argc), we can
+ *        do this in the function declaration
+ *        int main(int __attribute__((unused)) argc, char *argv[])
  * @argv: argument vector - argment passed to main as an array or strings
  * Return: 0
  */
 
 int main(int __attribute__((unused)) argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	(void)argc;
+
+	printf("%s\n", argv[0];
 
 	return (0);
 }
