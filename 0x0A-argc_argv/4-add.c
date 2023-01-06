97ca4f0a8179b7f20b7acc2487665406ed4b6d5e
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-01-06 15:39:52 -0800

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - add 2 positive numbers and print the result
+ * @argc: argument count
+ * @argv: argument vector, array of strings
+ * Description: If no number is passed to program, print 0
+ *              If one of the numbers contain non-digits, print Error
+ * Return: 1 if error, 0 if function runs properly
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, n, sum = 0;
+	char *p;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; argv[i]; i++)
+	{
+		n = strtol(argv[i], &p, 10);
+		if (*p)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
