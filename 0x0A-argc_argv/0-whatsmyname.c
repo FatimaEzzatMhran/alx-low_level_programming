80cef14511869d2fcbb59fe945d156eb4d02346a
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-01-06 13:26:18 -0800

    :0x09-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the name of the file followed by a new line
+ * @argc: argument count
+ * @argv: argument vector - argment passed to main as an array or strings
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
