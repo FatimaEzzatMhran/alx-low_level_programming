6f42c6fb6f3c171e62e823d3436867b3674edeb3
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-01-12 13:29:28 -0800

    string nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -12,20 +12,21 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	int strlen1, i, c;
+	int i, c;
+	unsigned int strlen1;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	strlen1 = (unsigned int)_strlen(s1);
+	strlen1 = _strlen(s1);
 	p = malloc((strlen1 + n + 1) * sizeof(char));
 	if (p == NULL)
 		return (NULL);
-	for (i = 0, c = 0; i < (strlen1 + n); i++)
+	for (i = 0, c = 0; (unsigned int) i < (strlen1 + n); i++)
 	{
-		if (i < strlen1)
+		if ((unsigned int) i < strlen1)
 			p[i] = s1[i];
 		else
 			p[i] = s2[c++];
@@ -41,9 +42,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
  * Return: length of string
  */
 
-int _strlen(char *s)
+unsigned int _strlen(char *s)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 		;
