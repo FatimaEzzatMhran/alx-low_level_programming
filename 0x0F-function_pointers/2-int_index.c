9f9ccfdeabd3668f899fb853d2462d4218acdd20
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-01-18 16:21:07 -0800

    int index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,11 +8,11 @@
  * Return: index of first element that matches with cmp or -1 if none found
  */
 
-int index(int *array, int size, int (*cmp)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0 || array == NULL || cmp == NULL)
+	if (size < 1 || array == NULL || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
