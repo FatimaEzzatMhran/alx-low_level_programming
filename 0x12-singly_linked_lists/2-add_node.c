5af9ab79dffd216253afb9084ef837ead1e8cc28
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-01-26 17:35:30 -0800

    linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s : s is a character
+ * Return: value is i
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * add_node - add a new node at the beginnig of list_t list
+ * @head: double pointer to head
+ * @str: string to be saved in new node, must be duplicated
+ * Return: Address of new element or NULL if failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *first_node;
+
+	if (head == NULL)
+		return (NULL);
+
+	first_node = (list_t *) malloc(sizeof(list_t));
+	if (first_node == NULL)
+	{
+		free(first_node);
+		return (NULL);
+	}
+
+	first_node->str = strdup(str);
+	first_node->len = _strlen(str);
+	first_node->next = *head;
+	*head = first_node;
+
+	return (*head);
+}
