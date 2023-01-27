897dae3183ee87ef08c5965e99ed803b751f551c
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-01-26 16:58:32 -0800

    linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -22,7 +22,7 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%d} %s\n", temp->len, temp->str);
+			printf("[%d] %s\n", temp->len, temp->str);
 		}
 		count++;
 		temp = temp->next;
