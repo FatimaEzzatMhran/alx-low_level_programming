200ae638cb53bed5304f0e2d90556b0e9a91151c
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-01-26 18:24:27 -0800

    linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - free a `list_t` list
+ * @head: head of linked list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+	list_t *nxt;
+
+	temp = head;
+
+	while (temp != NULL)
+	{
+		nxt = temp->nexr;
+		free(temp->str);
+		free(temp);
+		temp = nxt;
+	}
+}
