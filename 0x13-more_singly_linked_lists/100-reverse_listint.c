6f521b3eda37be215af098fbd9c890a4fbe84a87
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-01-31 04:17:58 -0800

    linked lists

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,7 +8,7 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *previous, *next;
+	listint_t *previous, *following;
 
 	if (head == NULL || *head == NULL)
 		return (NULL);
@@ -17,10 +17,10 @@ listint_t *reverse_listint(listint_t **head)
 	previous = NULL;
 	while (*head != NULL)
 	{
-		next = (*head)->next;
+		following = (*head)->next;
 		(*head)->next = previous;
 		previous = *head;
-		*head = next;
+		*head = following;
 	}
 	*head = previous;
 	return (*head);
