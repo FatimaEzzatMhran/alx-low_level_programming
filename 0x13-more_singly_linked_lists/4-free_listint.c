14fef3cd2a8385d5323f3e6c646296cfac550872
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-01-30 22:17:32 -0800

    linked lists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - free a `listint_t` list
+ * @head: head of linked list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
