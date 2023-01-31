5c7383e6198285b1329deeab57219d796bc47efe
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-01-31 01:47:31 -0800

    linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -44,7 +44,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (temp == NULL)
 			return (NULL);
 	}
-	new->next = temp->next;
-	temp->next = new;
+
+	if (temp->next)
+	{
+		new->next = temp->next;
+		temp->next = new;
+	}
+	else
+	{
+		new->next = NULL;
+		temp->next = new;
+	}
 	return (new);
 }
