937aad7baf5f82ef6b12183724b8e5bc2a98e8ec
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-02-06 15:20:52 -0800

    file I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,6 +13,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+		return (1);
+
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len] != '\0'; len++)
