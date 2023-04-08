0012338755d55b4642b1320eeb326428a9ff8df0
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-04-07 18:29:23 -0700

    hash table set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,13 +1,10 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_set - adds an element to the hash table.
  * @ht: the hash table you want to add or update the key/value to
  * @key: key to add - can't be an empty string
  * @value: the value associated with the key.
- *
  * Return: 1 if successful, 0 otherwise
- * Note: In case of collisions, add new node at the beginnig of the list
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -28,6 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			if (strcmp(item->key, key) == 0) /* update the value */
 			{
+				free(ht->array[index]->value);
 				item->value = value_copy;
 				return (1); /* success */
 			}
@@ -37,10 +35,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_item = malloc(sizeof(hash_node_t)); /* new item */
 	if  (new_item == NULL)
 	{
+		free(value_copy);
 		return (0); /* fail */
 	}
 	new_item->key = key_copy;
 	new_item->value = value_copy;
+	if (new_item->key == NULL)
+	{
+		free(new_item);
+		return (0);
+	}
 	new_item->next = NULL;
 	if (ht->array[index] != NULL)
 		new_item->next = ht->array[index];
