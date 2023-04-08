9045036c269b6851b73406166db19ff69ebe0cd4
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-04-07 18:11:09 -0700

    deletet hash table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,21 +8,21 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_table_t *head = ht;
-	hash_node_t *item, *temp;
+	hash_node_t *node, *tmp;
 	unsigned long int i;
 
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
 		{
-			item = ht->array[i];
-			while (item != NULL)
+			node = ht->array[i];
+			while (node != NULL)
 			{
-				temp = item->next;
-				free(item->key);
-				free(item->value);
-				free(item);
-				item = temp;
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
 			}
 		}
 	}
