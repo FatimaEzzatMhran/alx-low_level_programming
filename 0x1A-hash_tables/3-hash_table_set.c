3e586bc9f7910a8f9283b6ab7117d0e42e4ad44c
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-04-07 19:01:46 -0700

    hash table set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,44 +8,43 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_item, *item;
-	unsigned long int index;
+		hash_node_t *new;
 	char *value_copy;
-	char *key_copy;
+	unsigned long int index, i;
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+
 	index = key_index((const unsigned char *)key, ht->size);
-	item = ht->array[index];
-	if (item != NULL) /* key exists */
+	for (i = index; ht->array[i]; i++)
 	{
-		while (item)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			if (strcmp(item->key, key) == 0) /* update the value */
-			{
-				free(ht->array[index]->value);
-				item->value = strdup(value);
-				return (1); /* success */
-			}
-			item = item->next;
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
 		}
 	}
-	new_item = malloc(sizeof(hash_node_t)); /* new item */
-	if  (new_item == NULL)
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
 	{
 		free(value_copy);
-		return (0); /* fail */
-	}
-	new_item->key = strdup(key);
-	new_item->value = strdup(value);
-	if (new_item->key == NULL)
-	{
-		free(new_item);
 		return (0);
 	}
-	new_item->next = NULL;
-	if (ht->array[index] != NULL)
-		new_item->next = ht->array[index];
-	ht->array[index] = new_item;
-	return (1); /* success */
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
+	return (1);
 }
